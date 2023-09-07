b681bd64184b65502be157f906d46690ad2412ba
enukane <enukane@glenda9.org>
2023-09-07 22:12:41 +0900

    init
    Signed-off-by: enukane <enukane@glenda9.org>

---
+++ src/main.rs
@@ -0,0 +1,35 @@
+#[macro_use] extern crate rocket;
+
+use std::net::IpAddr;
+use rocket::http::Status;
+use rocket::request::{Request, Outcome, FromRequest};
+
+struct ClientAddr {ip: IpAddr}
+
+#[derive(Debug)]
+enum ClientAddrError {
+    Invalid,
+}
+
+#[rocket::async_trait]
+impl<'r> FromRequest<'r> for ClientAddr {
+    type Error = ClientAddrError;
+
+    async fn from_request(req: &'r Request<'_>) -> Outcome<Self, Self::Error> {
+        match req.client_ip()  {
+            None => Outcome::Failure((Status::InternalServerError, ClientAddrError::Invalid)),
+            Some(addr) => Outcome::Success(ClientAddr{ip: addr}),
+        }
+    }
+}
+
+#[get("/")]
+fn index(addr: ClientAddr) -> String {
+    let addr_str = addr.ip.to_string();
+    return addr_str
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index])
+}
